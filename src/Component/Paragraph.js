41d911f0b8906b9e089b2b345ccb1f73640e8022
AmanAsharma070320 <asharma070320@gmail.com>
2023-12-29 16:37:23 +0530

    Day5 HomeWork

---
+++ src/Component/Paragraph.js
@@ -0,0 +1,53 @@
+import { useState } from "react"
+import './Para'
+import Para from "./Para";
+function Paragraph(){
+
+    let[Value,setValue]= useState(0)
+    let[Arr,setArr] = useState([]);
+
+    function getNumber(e){
+        let storeValue = e.target.value;
+        if(storeValue>=0){
+            setValue(storeValue)
+        }
+    }
+    
+    function clickBtn(){
+       if(Value<1){
+        setArr([]);
+        alert("AMan")
+       }else if(Value>Para.length){
+        setArr([]);
+        alert("Lucky")
+       }else{
+        setArr(
+            Para.map((val,index)=>{
+                if(index<=Value-1){
+                    return val
+                }
+            })
+        )
+       }
+    }
+    console.log(Value);
+    return(
+        <div className="container">
+            <h1 className="line">TIRED OF BORING LOREM IPSUM?</h1>
+            <br />
+            <div className="content">
+                <h3 className="boldd">Paragraphs :</h3>
+                <input type="number" className="side" value={Value} min={1} max={8} onChange={getNumber}/>
+                <button onClick={clickBtn} className="btn">GENERATE</button>
+            </div>
+            <div className="shift">
+            {
+                Arr.map((list)=>{
+                    return <p> {list}</p>
+                })
+            }
+            </div>
+        </div>
+    );
+}
+export default Paragraph
